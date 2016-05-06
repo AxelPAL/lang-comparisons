a78d85ccaca9cae3ad89fb9ebd960899bf849623
AxelPAL <axelpal@gmail.com>
2016-05-06 11:32:22 +0500

    django static files

--- frameworks/ror/db/schema.rb
+++ frameworks/ror/db/schema.rb
@@ -13,9 +13,47 @@
 
 ActiveRecord::Schema.define(version: 20150814095648) do
 
+  create_table "django_migrations", force: :cascade do |t|
+    t.string   "app",     limit: 255, null: false
+    t.string   "name",    limit: 255, null: false
+    t.datetime "applied",             null: false
+  end
+
+  create_table "migrations", id: false, force: :cascade do |t|
+    t.string  "migration", limit: 255, null: false
+    t.integer "batch",     limit: 4,   null: false
+  end
+
+  create_table "password_resets", id: false, force: :cascade do |t|
+    t.string   "email",      limit: 255, null: false
+    t.string   "token",      limit: 255, null: false
+    t.datetime "created_at",             null: false
+  end
+
+  add_index "password_resets", ["email"], name: "password_resets_email_index", using: :btree
+  add_index "password_resets", ["token"], name: "password_resets_token_index", using: :btree
+
   create_table "text", force: :cascade do |t|
     t.string "title",   limit: 255
     t.text   "content", limit: 65535
   end
+
+  create_table "text1", force: :cascade do |t|
+    t.string   "title",     limit: 255
+    t.string   "content",   limit: 255
+    t.datetime "createdAt"
+    t.datetime "updatedAt"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",           limit: 255, null: false
+    t.string   "email",          limit: 255, null: false
+    t.string   "password",       limit: 60,  null: false
+    t.string   "remember_token", limit: 100
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "users", ["email"], name: "users_email_unique", unique: true, using: :btree
 
 end
